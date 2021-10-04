8e1cd808996c44f96fc0fd4896f09b89e5287ad1
Daniel <dvorhaug@gmail.com>
2021-10-04 09:34:39 +0200

    Basic clock

--- verilog/pixelArray.v
+++ verilog/pixelArray.v
@@ -1,6 +1,6 @@
 
 
-module pixelArray(
+module PIXEL_ARRAY(
     input logic POWER_ENABLE,
     input logic WRITE_ENABLE,
     input logic COUNTER_RESET,
@@ -10,8 +10,22 @@ module pixelArray(
     input logic ERASE,       // Pixel reset in paper
     input logic EXPOSE,      // PG in paper
 
-    inout [7:0] DATA,
+    inout wire [7:0] DATA
 
 );
+
+logic [7:0] counter = '0;
+
+// always_ff @(posedge COUNTER_CLOCK || posedge COUNTER_RESET) begin
+//     if (COUNTER_RESET) begin
+//         counter = 0;
+//     end else begin
+//         counter++;
+//     end
+// end
+
+always_ff @(posedge COUNTER_CLOCK ) begin
+    counter++;
+end
 
 endmodule
