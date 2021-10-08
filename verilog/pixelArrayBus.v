140ecd23b805dda7967b1bcff61c99f350e5b06a
Carl Thyness <carl.thyness@gmail.com>
2021-10-08 12:15:59 +0200

    Functioning simple bus with buffer

---
+++ verilog/pixelArrayBus.v
@@ -0,0 +1,17 @@
+module pixelArrayBus (
+    input logic [WIDTH*8-1:0] DATA,
+    input logic READ_CLK,
+    output logic [15:0] OUT
+);
+
+parameter integer WIDTH = 2;
+logic [WIDTH*8-1:0] buffer = '0;
+
+always_ff @(posedge READ_CLK) begin
+    buffer <= DATA;
+end
+always_comb begin
+    OUT = buffer;
+end
+
+endmodule
---
+++ verilog/pixelArrayBus_tb.v
@@ -0,0 +1,32 @@
+`timescale 1ns/1ps
+
+module pixelArrayBus_tb ();
+
+
+parameter integer WIDTH = 2;
+logic READ_CLK = 0;
+logic [WIDTH*8-1:0] INPUT = '0;
+wire [15:0] OUT = '0;
+parameter integer sim_period = 300;
+
+pixelArrayBus #(.WIDTH(WIDTH)) pab1(
+    .DATA(INPUT), .READ_CLK(READ_CLK), .OUT(OUT)
+    );
+
+initial
+    begin
+        $dumpfile("simulation/pixelArrayBus_tb.vcd");
+        $dumpvars(0, pixelArrayBus_tb);
+        INPUT = 69;
+        #sim_period
+        READ_CLK = 1;
+        #10
+        READ_CLK = 0;
+        #sim_period
+        INPUT = 0;
+        #sim_period
+        $stop;
+    end
+
+
+endmodule
