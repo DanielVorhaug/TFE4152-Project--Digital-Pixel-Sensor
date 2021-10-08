7d61e2b89457b36e70d9289521389a465fa22b51
Daniel <dvorhaug@gmail.com>
2021-10-08 10:55:21 +0200

    Counter comb and ff separation working

--- verilog/pixelArrayCounter.v
+++ verilog/pixelArrayCounter.v
@@ -5,15 +5,17 @@ module PIXEL_ARRAY_COUNTER (
     output logic [7:0] DATA 
 );    
 
+    logic [7:0] counter = '0;
+
     always_comb begin
-        DATA++;
+        counter = DATA + 1;
     end
 
     always_ff @(posedge COUNTER_CLOCK or posedge COUNTER_RESET) begin
         if (COUNTER_RESET) begin
-            DATA = 0;
+            DATA <= 0;
         end else begin
-            DATA = DATA;
+            DATA <= counter;
         end
     end
     
