25ee9e9584643f17116c0506c62727d4bc40a2d2
Carl Thyness <carl.thyness@gmail.com>
2021-10-11 10:53:19 +0200

    Wrote potential code for sequential read.

--- verilog/pixelArrayBus.v
+++ verilog/pixelArrayBus.v
@@ -1,15 +1,24 @@
 module PIXEL_ARRAY_BUS (
     input logic [WIDTH*8-1:0] DATA,
     input logic READ_CLK,
+    //input logic WRITE_CLK,
     output logic [15:0] OUT
 );
 
 parameter integer WIDTH = 2;
+//integer n = 0;
 logic [WIDTH*8-1:0] buffer = '0;
 
 always_ff @(posedge READ_CLK) begin
     buffer <= DATA;
 end
+
+/*always_ff @(posedge WRITE_CLK) begin //Potential code for sequential read
+    OUT = buffer[16*n - 1: 16*(n-1)];
+    n++;
+    if (2n==WIDTH) n = 0;
+end*/ 
+
 always_comb begin
     OUT = buffer;
 end
