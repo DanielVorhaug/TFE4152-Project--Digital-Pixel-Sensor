cf6ac958c64950001a80a21745003f808c21f055
Daniel <dvorhaug@gmail.com>
2021-10-15 13:47:26 +0200

    Memory Controller works

--- verilog/pixelArrayMemoryController.v
+++ verilog/pixelArrayMemoryController.v
@@ -2,35 +2,31 @@ module PIXEL_ARRAY_MEMORY_CONTROLLER (
     input logic SYSTEM_CLK,
     input logic READ_RESET,
     input logic READ_CLK_IN,
-    output logic READ_CLK_OUT,
+    output logic READ_CLK_OUT = 0,
     output logic WRITE_CLK_OUT,
-    output logic MEMORY_ROW,
-    output logic MEMORY_READ_ENABLE
+    output logic [$clog2(HEIGHT):0] MEMORY_ROW = -1,
+    output logic MEMORY_READ_ENABLE = 0
+    // Maybe add output to state machine to tell when it's done reading
 );
 
     parameter integer WIDTH = 2;
     parameter integer HEIGHT = 2;
-    parameter integer log2_height = $clog2(HEIGHT);
+    parameter integer OUTPUT_BUS_PIXEL_WIDTH = 2; // Needs to go up in WIDTH
 
     parameter integer read_to_buffer = 0, write_out = 1, done = 2;
 
-    
-
-        // Might not be needed
-    READ_CLK_OUT <= 0;
-    WRITE_CLK_OUT <= ;
-    MEMORY_ROW <= -1; 
-    MEMORY_READ_ENABLE <= 0;
-
-    logic state_enable <= 0;
+    logic [1:0] state = done;
     logic control_clk;
-    logic write_out_enable <= 0; 
+    logic state_enable = 0;
+    logic write_out_enable = 0; 
 
-    logic [log2_height:0] write_number = 0;
+    logic [$clog2(WIDTH):0] write_number = 0;
 
     always_ff @(posedge READ_CLK_IN) begin
         state_enable <= 1;
+        state <= read_to_buffer;
         MEMORY_READ_ENABLE <= 1;
+        MEMORY_ROW = -1;
     end
 
     assign control_clk = state_enable ? SYSTEM_CLK : 0;
@@ -41,24 +37,24 @@ module PIXEL_ARRAY_MEMORY_CONTROLLER (
         case (state)
             read_to_buffer:
             begin
+                write_out_enable <= 0;
+                write_number <= 0;
+
                 MEMORY_ROW <= MEMORY_ROW + 1;
                 READ_CLK_OUT <= 1;
                 state <= write_out;
                 write_number <= 0;
-
-                if (MEMORY_ROW == HEIGHT) state = done;
-
             end
 
             write_out:
             begin
+                READ_CLK_OUT <= 0;
                 write_out_enable <= 1;
                 write_number <= write_number + 1;
 
-                if (write_number[log2_height - 1])
+                if (write_number == WIDTH/OUTPUT_BUS_PIXEL_WIDTH - 1)
                 begin
-                    state <= read_to_buffer;
-                    write_number <= 0;
+                    state <= (MEMORY_ROW == HEIGHT - 1) ? done : read_to_buffer;
                 end
             end
 
@@ -67,8 +63,6 @@ module PIXEL_ARRAY_MEMORY_CONTROLLER (
                 MEMORY_READ_ENABLE = 0;
                 MEMORY_ROW = -1;
                 state_enable = 0;
-                state = read_to_buffer;
-
             end
 
             default: 
@@ -77,10 +71,5 @@ module PIXEL_ARRAY_MEMORY_CONTROLLER (
             end
         endcase
 
-
     end
-
-
-
-
 endmodule
--- verilog/pixelArrayMemoryController_tb.v
+++ verilog/pixelArrayMemoryController_tb.v
@@ -3,21 +3,25 @@
 module PIXEL_ARRAY_MEMORY_CONTROLLER_tb ();
 
 
-    logic SYSTEM_CLK;
+    logic SYSTEM_CLK = 0;
     logic READ_RESET = 0;
     logic READ_CLK_IN = 0;
     logic READ_CLK_OUT;
     logic WRITE_CLK_OUT;
-    logic MEMORY_ROW;
+    logic [$clog2(HEIGHT):0] MEMORY_ROW;
     logic MEMORY_READ_ENABLE;
 
+    parameter integer WIDTH = 8;
+    parameter integer HEIGHT = 9;
+    parameter integer OUTPUT_BUS_PIXEL_WIDTH = 4; 
+
     parameter integer clk_period = 500;
-    parameter integer sim_end = clk_period*2400;
+    parameter integer sim_period = clk_period*2400;
     always #clk_period SYSTEM_CLK=~SYSTEM_CLK;
 
     
 
-    PIXEL_ARRAY_MEMORY_CONTROLLER #(.HEIGHT(2)) pamc1(
+    PIXEL_ARRAY_MEMORY_CONTROLLER #(.HEIGHT(HEIGHT), .WIDTH(WIDTH), .OUTPUT_BUS_PIXEL_WIDTH(OUTPUT_BUS_PIXEL_WIDTH)) pamc1(
         .SYSTEM_CLK(SYSTEM_CLK),
         .READ_RESET(READ_RESET),
         .READ_CLK_IN(READ_CLK_IN),
@@ -36,6 +40,14 @@ module PIXEL_ARRAY_MEMORY_CONTROLLER_tb ();
             $dumpfile("simulation/PIXEL_ARRAY_MEMORY_CONTROLLER_tb.vcd");
             $dumpvars(0,PIXEL_ARRAY_MEMORY_CONTROLLER_tb);
 
+            #sim_period
+
+            READ_CLK_IN = 1;
+            #clk_period
+            READ_CLK_IN = 0;
+
+            #sim_period
+                $stop;
 
         end
 endmodule
