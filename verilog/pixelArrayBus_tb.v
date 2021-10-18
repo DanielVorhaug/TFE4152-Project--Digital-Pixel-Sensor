b517a20eb01628016df90e54bd5c0c78cd5c1714
Daniel <dvorhaug@gmail.com>
2021-10-18 14:41:30 +0200

    Update testbench

--- verilog/pixelArrayBus_tb.v
+++ verilog/pixelArrayBus_tb.v
@@ -4,14 +4,24 @@ module PIXEL_ARRAY_BUS_tb ();
 
 
 parameter integer WIDTH = 2;
-logic READ_CLK = 0;
-logic [WIDTH*8-1:0] INPUT;
-wire [15:0] OUT;
-parameter integer sim_period = 300;
+parameter integer HEIGHT = 2;
+parameter integer OUTPUT_BUS_PIXEL_WIDTH = 2;
+parameter integer BIT_DEPTH = 8;
+integer output_bus_width = OUTPUT_BUS_PIXEL_WIDTH*BIT_DEPTH;
 
-PIXEL_ARRAY_BUS #(.WIDTH(WIDTH)) pab1(
-    .DATA(INPUT), .READ_CLK(READ_CLK), .OUT(OUT)
-    );
+logic READ_CLK = 0;
+logic WRITE_CLK = 0;
+logic [output_bus_width-1:0] INPUT;
+wire [output_bus_width-1:0] OUT;
+
+parameter integer clk_period = 500;
+parameter integer sim_period = clk_period *10.2;
+// always #clk_period READ_CLK=~READ_CLK;
+// always #clk_period WRITE_CLK=~WRITE_CLK;
+
+PIXEL_ARRAY_BUS #(.WIDTH(WIDTH), .OUTPUT_BUS_PIXEL_WIDTH(OUTPUT_BUS_PIXEL_WIDTH), .BIT_DEPTH(BIT_DEPTH)) pab1(
+    .DATA(INPUT), .READ_CLK(READ_CLK), .WRITE_CLK(WRITE_CLK), .OUT(OUT)
+);
 
 initial
     begin
@@ -20,6 +30,8 @@ initial
         INPUT = 69;
         #sim_period
         READ_CLK = 1;
+        #10
+        WRITE_CLK = 1;
         #10
         READ_CLK = 0;
         #sim_period
