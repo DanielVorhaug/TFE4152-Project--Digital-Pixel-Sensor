adaa6cef8dfff3f0b18f31f619607954672a038f
Daniel <dvorhaug@gmail.com>
2021-10-25 14:12:03 +0200

    Copied in tb from ps_tb

--- verilog/pixelArray_tb.v
+++ verilog/pixelArray_tb.v
@@ -42,6 +42,137 @@ module pixelArray_tb;
         .DATA_OUT ()
         );
 
+    
+    //------------------------------------------------------------
+   // State Machine
+   //------------------------------------------------------------
+   parameter ERASE=0, EXPOSE=1, CONVERT=2, READ=3, IDLE=4;
+
+   logic               convert_stop;
+   logic [2:0]         state,next_state;   //States
+   integer           counter;            //Delay counter in state machine
+
+   //State duration in clock cycles
+   parameter integer c_erase = 5;
+   parameter integer c_expose = 255;
+   parameter integer c_convert = 255;
+   parameter integer c_read = 5;
+
+   // Control the output signals
+   always_ff @(negedge clk ) begin
+      case(state)
+        ERASE: begin
+           erase <= 1;
+           read <= 0;
+           expose <= 0;
+           convert <= 0;
+        end
+        EXPOSE: begin
+           erase <= 0;
+           read <= 0;
+           expose <= 1;
+           convert <= 0;
+        end
+        CONVERT: begin
+           erase <= 0;
+           read <= 0;
+           expose <= 0;
+           convert = 1;
+        end
+        READ: begin
+           erase <= 0;
+           read <= 1;
+           expose <= 0;
+           convert <= 0;
+        end
+        IDLE: begin
+           erase <= 0;
+           read <= 0;
+           expose <= 0;
+           convert <= 0;
+
+        end
+      endcase // case (state)
+   end // always @ (state)
+
+
+   // Control the state transitions
+   //TODO: The counter should probably be an always_comb. Might be a good idea
+   //to also reset the counter from the state machine, i.e provide the count
+   //down value, and trigger on 0
+   always_ff @(posedge clk or posedge reset) begin
+      if(reset) begin
+         state = IDLE;
+         next_state = ERASE;
+         counter  = 0;
+         convert  = 0;
+      end
+      else begin
+         case (state)
+           ERASE: begin
+              if(counter == c_erase) begin
+                 next_state <= EXPOSE;
+                 state <= IDLE;
+              end
+           end
+           EXPOSE: begin
+              if(counter == c_expose) begin
+                 next_state <= CONVERT;
+                 state <= IDLE;
+              end
+           end
+           CONVERT: begin
+              if(counter == c_convert) begin
+                 next_state <= READ;
+                 state <= IDLE;
+              end
+           end
+           READ:
+             if(counter == c_read) begin
+                state <= IDLE;
+                next_state <= ERASE;
+             end
+           IDLE:
+             state <= next_state;
+         endcase // case (state)
+         if(state == IDLE)
+           counter = 0;
+         else
+           counter = counter + 1;
+      end
+   end // always @ (posedge clk or posedge reset)
+
+   //------------------------------------------------------------
+   // DAC and ADC model
+   //------------------------------------------------------------
+   logic[7:0] data;
+
+   // If we are to convert, then provide a clock via anaRamp
+   // This does not model the real world behavior, as anaRamp would be a voltage from the ADC
+   // however, we cheat
+   assign anaRamp = convert ? clk : 0;
+
+   // During expoure, provide a clock via anaBias1.
+   // Again, no resemblence to real world, but we cheat.
+   assign anaBias1 = expose ? clk : 0;
+
+   // If we're not reading the pixData, then we should drive the bus
+   assign pixData = read ? 8'bZ: data;
+
+   // When convert, then run a analog ramp (via anaRamp clock) and digtal ramp via
+   // data bus.
+   always_ff @(posedge clk or posedge reset) begin
+      if(reset) begin
+         data =0;
+      end
+      if(convert) begin
+         data +=  1;
+      end
+      else begin
+         data = 0;
+      end
+   end
+
 
 
     initial
