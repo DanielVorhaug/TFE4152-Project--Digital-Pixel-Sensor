42e53a5383b859156f6fe32873571d1eb052441a
Daniel <dvorhaug@gmail.com>
2021-10-29 14:19:51 +0200

    Defined IO

---
+++ verilog/pixelTop.v
@@ -0,0 +1,10 @@
+
+
+module PIXEL_TOP (
+    input  logic SYSTEM_CLK,
+    input  logic SYSTEM_RESET,
+    output logic DATA_OUT_CLK,
+    output logic [OUTPUT_BUS_PIXEL_WIDTH*BIT_DEPTH-1:0] DATA_OUT
+);
+    
+endmodule
