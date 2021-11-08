c6c5318b2b0b5296979756e3834698b73d242853
Carl Thyness <carl.thyness@gmail.com>
2021-11-08 13:09:36 +0100

    Made a PISO BUS

--- verilog/pixelArrayBus_tb.v
+++ verilog/pixelArrayBus_tb.v
@@ -3,21 +3,25 @@
 module PIXEL_ARRAY_BUS_tb ();
 
 
-parameter integer WIDTH = 2;
+parameter integer BIT_DEPTH = 8;
+parameter integer OUTPUT_BUS_PIXEL_WIDTH = 2;
+parameter integer WIDTH = 4;
 logic READ_CLK = 0;
 logic [WIDTH*8-1:0] INPUT;
 wire [15:0] OUT;
 parameter integer sim_period = 300;
+logic WRITE_CLK = 0;
+always #10 WRITE_CLK = ~WRITE_CLK;
 
-PIXEL_ARRAY_BUS #(.WIDTH(WIDTH)) pab1(
-    .DATA(INPUT), .READ_CLK(READ_CLK), .OUT(OUT)
+PIXEL_ARRAY_BUS #(.WIDTH(WIDTH), .BIT_DEPTH(BIT_DEPTH), .OUTPUT_BUS_PIXEL_WIDTH(OUTPUT_BUS_PIXEL_WIDTH)) pab1(
+    .DATA(INPUT), .READ_CLK(READ_CLK), .OUT(OUT), .WRITE_CLK(WRITE_CLK)
     );
 
 initial
     begin
         $dumpfile("simulation/PIXEL_ARRAY_BUS_tb.vcd");
         $dumpvars(0, PIXEL_ARRAY_BUS_tb);
-        INPUT = 69;
+        INPUT = 'h10325623;
         #sim_period
         READ_CLK = 1;
         #10
