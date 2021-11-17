b87e545fc232881bdeb09f444dcdd83ba09eba89
Daniel <dvorhaug@gmail.com>
2021-11-17 15:28:45 +0100

    Gray decoder in bus

--- verilog/pixelArrayBus.v
+++ verilog/pixelArrayBus.v
@@ -9,15 +9,37 @@ parameter integer BIT_DEPTH = 8;
 parameter integer OUTPUT_BUS_PIXEL_WIDTH = 2;
 parameter integer WIDTH = 2;
 logic [WIDTH*BIT_DEPTH-1:0] buffer = '0;
+logic [OUTPUT_BUS_PIXEL_WIDTH*BIT_DEPTH-1:0] converted;
 
 always_ff @(posedge READ_CLK) begin
     buffer <= DATA;
 end
 
 always_ff @(posedge WRITE_CLK) begin
-    OUT <= buffer[BIT_DEPTH*OUTPUT_BUS_PIXEL_WIDTH-1: 0];
+    OUT <= converted;
 end
 
+
+
+    // Gray decoder: Turns Gray code into regular binary
+genvar pixel;
+genvar index;
+generate
+    
+    for (pixel = 1; pixel < OUTPUT_BUS_PIXEL_WIDTH+1; pixel++) begin
+        assign converted[pixel*BIT_DEPTH-1] = buffer[pixel*BIT_DEPTH-1];
+
+        for (index = 2; index < BIT_DEPTH+1; index++) begin
+            assign converted[pixel*BIT_DEPTH - index] = converted[pixel*BIT_DEPTH - index+1] ^ buffer[pixel*BIT_DEPTH - index];
+        end
+    end
+       
+endgenerate
+
+
+
+
+    // Shift register
 genvar i;
 generate for(i = 0; i < WIDTH/OUTPUT_BUS_PIXEL_WIDTH-1; i++) begin
         always_ff @(posedge WRITE_CLK) begin
